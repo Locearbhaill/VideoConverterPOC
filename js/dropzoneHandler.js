3fbea5b4a73ca3cce06d730358c89eb66c7602ac
Liam Cearbhaill <FZL7HM@EUR.corp.gm.com>
2020-09-23 13:18:43 +0100

    Added drag and drop, upload doesn't work however

--- js/dropzoneHandler.js
+++ js/dropzoneHandler.js
@@ -2,10 +2,30 @@
     // console.log("test");
     var dropzone = document.getElementById('dropzone'); // select element by your given name
 
+    var upload = function(files) {
+        var formData = new FormData(),
+            xhr = new XMLHttpRequest(),
+            x;
+
+        for (x=0; x < files.length; x = x + 1 ) {
+            formData.append('file[]', files[x]); // Add all files to upload
+        }
+
+        xhr.onload = function() {
+            var data = this.responseText; // response from ajax request
+            console.log(data);
+
+        }
+
+        xhr.open('post', '../upload.php'); // post to php VideoConverterPOC\php\upload.php 
+        xhr.send(formData); // post data to php
+
+    }
+
     dropzone.ondrop = function(e) { // e for event
         e.preventDefault(); // Prevent browser from instantly opening uploaded file
         this.className = 'dropzone';
-        // upload(e.dataTransfer.files);
+        upload(e.dataTransfer.files);
     };
 
     dropzone.ondragover = function() { // set the dragover behaviour 
